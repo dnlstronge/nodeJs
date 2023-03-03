35e847b37984cf5417cfe88b4d006cebbf8511e3
Dan Stronge <dnlstronge@gmail.com>
2023-03-03 11:12:44 +0000

    [x]  - create a simple node app

--- App.ts
+++ App.ts
@@ -5,7 +5,7 @@ const person =  {
     age: 25
 }
 
-const amount = 12;
+const amount = 9;
 
 if(amount < 10) {
     console.log("smaller")
--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var person = {
     name: "dave",
     age: 25
 };
-var amount = 12;
+var amount = 9;
 if (amount < 10) {
     console.log("smaller");
 }
