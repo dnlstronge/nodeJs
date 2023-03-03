4b0c0dd241355ae0486725b38024597153df44be
Dan Stronge <dnlstronge@gmail.com>
2023-03-03 08:37:06 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const { EventEmitter } = require('events');
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('lunch', () => {
+    console.log("itchy tasty!!")
+})
+eventEmitter.on("breakfast", () => {
+    console.log("Scratchy itchy tasty....")
+})
+
+eventEmitter.emit('lunch');
+eventEmitter.emit("breakfast")
