fd27fe64a412f3b2c16c88371d1787ba371d9735
Dan Stronge <dnlstronge@gmail.com>
2023-03-03 10:05:42 +0000

    [x] - create simple function to be called when a condition is met based on data from an imported file, [x] - change the data in the file to get a different output

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import daves from "./new-module.js";
 
 const name = daves.person;
+let numero = 0
+const myFunction = () => {
+    numero = 55
+}
 
+if(name === "dave") {
+    myFunction()
+    console.log(numero)
+} else {
+    console.log("the name isn't dave")
+}
 
 console.log(name);
 
