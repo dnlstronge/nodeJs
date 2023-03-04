11c9deb4583f21adbb461c7978c8470dcc2aaf55
Dan Stronge <dnlstronge@gmail.com>
2023-03-04 10:05:20 +0000

    [x] - use OS to log uptime;

--- modules.js
+++ modules.js
@@ -2,4 +2,12 @@ const os = require("os")
 
 // info about current user: 
 const user = os.userInfo()
-console.log(user)
+console.log(user);
+
+// get info about how long machine has been running
+
+const runTime = os.uptime()
+
+console.log(runTime);
+
+console.log(Number((runTime / 1000 / 60).toFixed(2))) // mins
