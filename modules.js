dd05e5880d25e9647e44774ac23d51b98cacde06
Dan Stronge <dnlstronge@gmail.com>
2023-03-04 10:27:47 +0000

    [x] - log object containing diff data about OS

--- modules.js
+++ modules.js
@@ -6,13 +6,22 @@ console.log(user);
 
 // get info about how long machine has been running
 
-
-
-
-
 const output = () => {
      let upTime = os.uptime()
      return `The system has been running for ${upTime} seconds or ${upTime / 60} minutes`
 }
 
 console.log(output()) // mins
+
+// log data about the OS: 
+
+
+
+const currentOS = {
+    name: os.type(),
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem()
+}
+
+console.log(currentOS)
