83a2d84c27d61f2ae12c8dde7c19aaff7d7a43f6
Dan Stronge <dnlstronge@gmail.com>
2023-03-04 09:25:22 +0000

    [x] - add comments

--- demonstrate.js
+++ demonstrate.js
@@ -1,2 +1,5 @@
 require("./app") // hmmm so doing this calls that function even though it's located
 // in another file and I didn't export it???? 
+
+/* The reason for this is that if you have invoked a function inside a module as in the example - when node wraps it
+it also invokes the function (i.e you havent placed it in a variable ===> wouldn't lose sleep over this though) */
