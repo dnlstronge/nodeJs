28224bad8079bf2e1c8e1c898eb35f94e2a9b087
Dan Stronge <dnlstronge@gmail.com>
2023-03-05 08:43:00 +0000

    [x] =- use .resolve() to return an absolute path

--- path.js
+++ path.js
@@ -10,7 +10,7 @@ console.log(filePath);
 
 /* outputs: /content/subfolder/test.txt */
 
-/* even if you add to this it will be removed e.g:  */
+/* even if you add to this it will be removed e.g: trailing slashes  */
 
 const filePath2 = path.join("/content", "//subfolder", "/test.txt")
 
@@ -22,3 +22,8 @@ console.log(filePath2) // returns the same as filePath
 const base = path.basename(filePath);
 
 console.log(base)
+
+// returns an absolute path: 
+
+const resolved = path.resolve(__dirname, "content", "subfolder", "test.txt");
+console.log(resolved);
