9a2d49153e48be472f8015cc76433660608f09a0
Dan Stronge <dnlstronge@gmail.com>
2023-03-05 09:26:54 +0000

    [x] - create file demo async

---
+++ asyncWrite.js
@@ -0,0 +1,27 @@
+// async:
+
+const { readFile, writeFile } = require("fs");
+
+// async needs a callback :)
+
+// whenever the functionality is complete run the callback, just like
+// add eventlistener on a button
+
+readFile("./content/third.txt", (err, result) => {
+    if(err) {
+        console.log("Whoops, something went wrong.....")
+        return;
+    }
+    console.log(result);
+});
+
+/* if you don't add encoding value you will get buffer data ;
+
+<Buffer 41 73 20 49 20 6e 6f 64 64 65 64 20 6e 65 61 72 6c 79 20 6e 
+61 70 70 69 6e 67 2e 2e 2e 2e 73 75 64 64 65 6e 6c 79 20 74 68 65 
+72 65 20 63 61 6d 65 20 ... 183 more bytes>
+
+*/
+
+
+
