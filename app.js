8d7a1ed22bbe10944bf92b0c67d9044a5b99791c
Dan Stronge <dnlstronge@gmail.com>
2023-03-05 10:23:30 +0000

    [x] - changes to folder & file structure [x] - new file to demo http module

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require("http");
+
+
+// create server takes a callback with 2 params: 
+// you can name the params (objects) whatever but a common practice is req + res
+
+const server = http.createServer((req, res) => {});
+
