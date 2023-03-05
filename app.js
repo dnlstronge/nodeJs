638f9d3beb9b00f8cac184dabd9614aa270aa125
Dan Stronge <dnlstronge@gmail.com>
2023-03-05 09:06:13 +0000

    [x] - cheeck file was created, create var and log (pass)

--- app.js
+++ app.js
@@ -8,5 +8,11 @@ const { readFileSync, writeFileSync, readFile } = require("fs");
 
 const first = readFileSync("./content/first.txt", "utf8");
 const second = readFileSync("./content/second.txt", "utf8");
+const third = readFileSync("./content/third.txt", "utf8");
 
-console.log(first, second);
+console.log(first, second, third);
+
+// okay now create a newfile using write: 
+// two args, path (if it does not exist Node will create), "value"
+
+writeFileSync("./content/third.txt", "As I nodded nearly napping....");
