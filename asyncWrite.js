c4dced4abdfe5053d9523b85ca2c5ad8b7745338
Dan Stronge <dnlstronge@gmail.com>
2023-03-05 09:29:02 +0000

    [x] - demo buffer and encoding added

--- asyncWrite.js
+++ asyncWrite.js
@@ -7,7 +7,7 @@ const { readFile, writeFile } = require("fs");
 // whenever the functionality is complete run the callback, just like
 // add eventlistener on a button
 
-readFile("./content/third.txt", (err, result) => {
+readFile("./content/third.txt", "utf8", (err, result) => {
     if(err) {
         console.log("Whoops, something went wrong.....")
         return;
@@ -20,6 +20,17 @@ readFile("./content/third.txt", (err, result) => {
 <Buffer 41 73 20 49 20 6e 6f 64 64 65 64 20 6e 65 61 72 6c 79 20 6e 
 61 70 70 69 6e 67 2e 2e 2e 2e 73 75 64 64 65 6e 6c 79 20 74 68 65 
 72 65 20 63 61 6d 65 20 ... 183 more bytes>
+
+*/
+
+// after encoding added as 2nd arg: 
+
+/*
+
+As I nodded nearly napping....suddenly there came a tapping.......
+as if someone gently rapping......rapping at my chamber door....
+tis some late visitor I muttered, entreating entrance at my chamber door
+....this it is and nothing more
 
 */
 
