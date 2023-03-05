57062bd6f460f58e9d885d9125ceb6b92a71f6c6
Dan Stronge <dnlstronge@gmail.com>
2023-03-05 10:45:00 +0000

    [x] - demo server running

--- app.js
+++ app.js
@@ -4,5 +4,14 @@ const http = require("http");
 // create server takes a callback with 2 params: 
 // you can name the params (objects) whatever but a common practice is req + res
 
-const server = http.createServer((req, res) => {});
+// req ===> incoming request
+// res ===> response of what is being sent back
 
+const server = http.createServer((req, res) => {
+    res.write('welcome to my homepage')
+    res.end()
+});
+
+//server needs to listen to a port: (number is arbitrary for now)
+
+server.listen(3001)
