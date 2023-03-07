bcf97ef3a7cb139ccebf30b6a99c8f0e667cde1c
Dan Stronge <dnlstronge@gmail.com>
2023-03-07 08:39:17 +0000

    [x] - create simple function demo event loop

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // taking a look at event loop in action
+// I'll order the number in which the tasks get executed
+
+const { readFile } = require("fs")
+// (1)
+console.log("log first start task");
+
+readFile("./content/first.txt", "utf8", (err, result) => {
+    if(err) {
+        console.log(err.message)
+        return
+    }
+    // (3)
+    console.log(result)
+    console.log("second task completed")
+
+})
+
+// (2)
+console.group("starting next task")
