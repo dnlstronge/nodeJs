106631d39686866051cd8408576fc21fd2b13a0c
Dan Stronge <dnlstronge@gmail.com>
2023-03-08 11:07:11 +0000

    [x] - demo util

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
-const { readFile } = require("fs");
+const { readFile, writeFile } = require("fs");
 const util = require("util");
 
-// util has promise()
+// util has promisify() to return promise. end result is to get promise back
 
 // now you I can invoke getText and it will return a promise
 // this is a behind the scenes for async-await
 
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, "utf8", (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
+const readFilePromise = util.promisify(readFile)
+const writeFilePromise = util.promisify(writeFile)
+
+
 
 const start = async () => {
   try {
-    const first = await getText("./content/first.txt");
-    const second = await getText("./content/second.txt");
+    const first = await readFilePromise("./content/first.txt", "utf8");
+    const second = await readFilePromise("./content/second.txt", "utf8");
     console.log(first, second); // only once promise is resolved get response
   } catch (error) {
     console.log(error);
@@ -32,4 +25,18 @@ const start = async () => {
 
 start();
 
+/* old code for reference:  */
+
 // gettext("./content/first.txt").then(result => console.log(result)).catch(err => console.log(err));
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
