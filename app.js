d6ffd4e73faa284fee3b0951630b348e75aa6482
Dan Stronge <dnlstronge@gmail.com>
2023-03-08 10:15:41 +0000

    [x] - add comment

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const { readFile } = require("fs");
 
 
 // now you I can invoke getText and it will return a promise
+// this is a behind the scenes for async-await
 
 const gettext = (path) => {
     return new Promise((resolve, reject) => {
