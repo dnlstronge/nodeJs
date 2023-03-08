dd3c36e8a62493f43b4dc08a8f8677a1d5820906
Dan Stronge <dnlstronge@gmail.com>
2023-03-08 11:12:44 +0000

    [x] - refactor read write to access promises on FS directly

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { readFile, writeFile } = require("fs");
+const { readFile, writeFile } = require("fs").promises
 const util = require("util");
 
 // util has promisify() to return promise. end result is to get promise back
@@ -6,15 +6,18 @@ const util = require("util");
 // now you I can invoke getText and it will return a promise
 // this is a behind the scenes for async-await
 
+
+/*
 const readFilePromise = util.promisify(readFile)
 const writeFilePromise = util.promisify(writeFile)
 
-
+*/
 
 const start = async () => {
   try {
-    const first = await readFilePromise("./content/first.txt", "utf8");
-    const second = await readFilePromise("./content/second.txt", "utf8");
+    const first = await readFile("./content/first.txt", "utf8");
+    const second = await readFile("./content/second.txt", "utf8");
+    await writeFile("./content/third.txt", "Eagerly I 'wait the morrow, vainly I had saught to borrow...")
     console.log(first, second); // only once promise is resolved get response
   } catch (error) {
     console.log(error);
