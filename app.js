029c44d214ca8ab27c476d73a5d09517303923ea
Dan Stronge <dnlstronge@gmail.com>
2023-03-08 10:38:39 +0000

    [x] - try throw error (clear)

--- app.js
+++ app.js
@@ -1,33 +1,30 @@
 const { readFile } = require("fs");
 
-
 // now you I can invoke getText and it will return a promise
 // this is a behind the scenes for async-await
 
 const getText = (path) => {
-    return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
+    readFile(path, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
 
-readFile(path, "utf8", (err, data) => {
-    if(err) {
-        reject(err)
-    }
-    else {
-        resolve(data)
-    }
-})
-    })
-}
+const start = async () => {
+  try {
+    const first = await getText("./content/first.txt");
+    console.log(first); // only once promise is resolved get response
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const start = async() => {
-    try {
-    const first = await getText("./content/first.txt")
-                  console.log(first) // only once promise is resolved get response
-    } catch(error) {
-        console.log("Error - file not found")
-    }
-}
-
-// invoke start: 
+// invoke start:
 
 start();
 
